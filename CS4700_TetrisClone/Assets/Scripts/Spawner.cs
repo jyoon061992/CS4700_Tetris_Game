0f9d7371cdd6b55b95055b5f59feda695a1acf92
nandanvinjamury <nvinjamury@gmail.com>
2019-03-17 14:03:35 -0700

    spawner object (without falling)

---
+++ CS4700_TetrisClone/Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] blocks;
+    int randomblock;
+    bool isBlockPlaced;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        isBlockPlaced = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnRandom();
+    }
+
+
+    void SpawnRandom()
+    {
+        if (isBlockPlaced) {
+            randomblock = Random.Range(0, blocks.Length);
+            Instantiate(blocks[randomblock], transform.position, Quaternion.identity);
+            isBlockPlaced = false;
+        }
+    }
+
+}
