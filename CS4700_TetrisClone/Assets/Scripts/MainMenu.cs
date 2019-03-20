07ed34c5d913eb4f376ebcee9087f186b4b91500
Jonathan Fong <robofin117@gmail.com>
2019-03-19 22:41:56 -0700

    Added MainMenu functionality. Options menu button doesn't do anything. Also made scripts for a pause menu.

--- CS4700_TetrisClone/Assets/Scripts/MainMenu.cs
+++ CS4700_TetrisClone/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu: MonoBehaviour
 {
-    bool options= false;
-    void OptionsSwitch()
+
+    bool Options= false;
+
+    void Start()
     {
 
     }
+
+    public void OptionsSwitch()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TetrisGame");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    
 }
