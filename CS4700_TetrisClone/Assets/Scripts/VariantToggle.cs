1a4ea40625dbfa2fd8a282d1b85e951404c913ec
nandanvinjamury <nvinjamury@gmail.com>
2019-03-24 23:02:50 -0700

    variant toggle

---
+++ CS4700_TetrisClone/Assets/Scripts/VariantToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariantToggle : MonoBehaviour
+{
+    public GameObject leftpanel;
+    public GameObject leftinversepanel;
+
+    public void Toggle()
+    {
+        if(Camera.main.transform.eulerAngles == Vector3.zero)
+            Camera.main.transform.eulerAngles = Vector3.zero + new Vector3(0, 0, 180);
+        else
+            Camera.main.transform.eulerAngles = Vector3.zero;
+
+
+        leftinversepanel.SetActive(!leftinversepanel.activeSelf);
+        leftpanel.SetActive(!leftpanel.activeSelf);
+
+    }
+
+
+
+}
